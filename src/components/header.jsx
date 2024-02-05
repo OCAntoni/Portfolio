058a331977af00b4d0f25919cffc4fa650b02698
Antoni Castagne <anto.casatgne@hotmail.fr>
2024-02-05 12:40:12 +0100

    git install and add sass

---
+++ src/components/header.jsx
@@ -0,0 +1,12 @@
+import Logo from "../assets/images/logo.webp"
+
+function Header() {
+    return (
+        <header>
+            <img src={Logo} alt="logo antoni castagné" />
+            <p> test test test test</p>
+        </header>
+    )
+}
+
+export default Header
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,6 +1,10 @@
+import Header from "../components/header"
+import "../scss/main.scss"
+
 function Home() {
     return (
         <>
+            <Header />
             <h1>Hello</h1>
         </>
     )
