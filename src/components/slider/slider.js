eeb11e7661eddf6ddab75b417d2b4e402a0c6a5e
Antoni Castagne <anto.casatgne@hotmail.fr>
2024-02-19 12:48:30 +0100

    add fonts + change slider

--- src/components/slider/slider.js
+++ src/components/slider/slider.js
@@ -1,37 +1,52 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 
 import dataProjets from '../../data/data.json'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faHandPointLeft } from '@fortawesome/free-solid-svg-icons'
-import { faHandPointRight} from '@fortawesome/free-solid-svg-icons'
+import { faArrowRightLong } from '@fortawesome/free-solid-svg-icons'
 
 function Slider({images}) {
-    const [currentIndex , setCurrentIndex]= useState(1);
-    console.log(dataProjets);
+    const [currentIndex , setCurrentIndex]= useState(0);
+    
+    useEffect(() => {
+        const interval = setInterval(() => {
+            next();
+        }, 3000);
 
-    function back() {
-        (currentIndex === 1) ? setCurrentIndex(currentIndex + (dataProjets.length-1)) : setCurrentIndex(currentIndex - 1);
-    }
+        return () => clearInterval(interval);
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [currentIndex]);
 
     function next() {
-        (currentIndex === dataProjets.length) ? setCurrentIndex(currentIndex - (dataProjets.length-1)) : setCurrentIndex(currentIndex + 1);
+        setCurrentIndex((prevIndex) => (prevIndex === dataProjets.length - 1 ? 0 : prevIndex + 1));
+    }
+
+    function goToProject(index) {
+        setCurrentIndex(index); 
     }
 
     return (
         <div id="news">
-            <h2>News</h2>
+            <h2 className="section-title">
+                Actualitées
+            </h2>
             <div className="slider-project">
-                <img className="img-slider" src={dataProjets[currentIndex-1].image} alt="" />
+                <img className="img-slider" src={dataProjets[currentIndex].image} alt="" />
                     {dataProjets.length > 1 ? (
-                    <div className="change-project">
-                        <FontAwesomeIcon icon={faHandPointLeft} className="change hand-right" onClick={()=> back()}/>
-                        <FontAwesomeIcon icon={faHandPointRight} className="change hand-left" onClick={() => next()}/>
-                        <h3>
-                            {dataProjets[currentIndex-1].title}
-                        </h3>
-                        <p className="counter-images">{currentIndex}/{dataProjets.length}</p>
-                    </div>
-                ):null }
+                        <div className="change-project">
+                            <button className="button-link">
+                                En savoir plus
+                                <FontAwesomeIcon className="icon" icon={faArrowRightLong} />
+                            </button>
+                            <div className="dots">
+                                {dataProjets.map((projet, index) => (
+                                <div key={index} 
+                                    className={index === currentIndex ? "dot active" : "dot"}
+                                    onClick={() => goToProject(index)}>
+                                </div>
+                                ))}
+                            </div>
+                        </div>
+                    ):null }
             </div>
         </div>
      );
