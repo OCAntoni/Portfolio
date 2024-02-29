a110a3d023e53c854cb1fde2f04e059d54b85899
Antoni Castagne <anto.casatgne@hotmail.fr>
2024-02-29 18:16:44 +0100

    minor changes

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -14,14 +14,14 @@ function About() {
                 />
                 <div className="text-about" style={{ lineHeight: '1.5' }}>
                     <p>
-                        Durant mes précédentes expériences professionelles, j'ai eu l'opportunité d'explorer plusieurs domaines. C'est ce qui me permet d'aborder chaque nouveau projet avec enthousiasme et compréhension, mais également d'une capacité d'écoute empathique. Celles ci me permettent de comprendre les besoins des autres.
+                        Durant mes précédentes expériences professionelles, j'ai eu l'opportunité d'explorer plusieurs domaines. C'est ce qui me permet d'aborder chaque nouveau projet avec enthousiasme tout en saisissant les enjeux. Je dispose également d'une capacité d'écoute qui est un élément primordial pour mener à bien mes differents projets. Ainsi je serai en mesure de comprendre les besoins de mes clients.
                         Mon intérêt pour la tech et l'informatique se traduit par ma passion pour le développement web et la construction de solution digitale.
                     </p>
                     <p>
-                        Je me suis naturellement tourné vers une formation de 9 mois avec Openclassrooms, qui m'a permis d'acquérir de nouvelles compétences, ainsi que de nouveaux outils de développement notamment. Cette formation étant entièrement à distance a pu renforcer mon autonomie et mon aptitude à naviguer seul dans le domaine du développement.
+                        Je me suis naturellement tourné vers une formation de 9 mois avec Openclassrooms, qui m'a permis d'acquérir de nouvelles compétences, notamment de nouveaux outils de développement. Cette formation entièrement à distance a pu renforcer mon autonomie et mon aptitude à naviguer seul dans le domaine du développement.
                     </p>
                     <p>
-                        N"hésitez pas à explorer mon protfolio plus en détails ou à me contacter, je suis toujours ouvert à de nouvelles colaborations ainsi qu'à de nouvelles opportunités.
+                        N'hésitez pas à explorer mon portfolio plus en détails ou à me contacter, je suis toujours ouvert à de nouvelles collaborations ainsi qu'à de nouvelles opportunités.
                     </p>
                     <div className="download">
                         <a href={process.env.PUBLIC_URL + '/cv_test.pdf'} download="CV_Castagne_Antoni.pdf">
